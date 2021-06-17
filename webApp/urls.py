d443fb09790513f4703d454702331e1b749d56a5
meetsanchitjain <meetsanchitjain@yahoo.com>
2021-06-17 18:16:54 +0530

    error solved

--- webApp/urls.py
+++ webApp/urls.py
@@ -32,6 +32,6 @@ urlpatterns = [
     path('search',views.search),
     path('admin/', admin.site.urls),
     path('', include("Home.urls")),
-    #url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}),
-    #url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
